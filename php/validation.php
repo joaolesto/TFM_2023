ed2576ec670330be78f588c9134c9144b0594ec4
joaolesto <joaolesto@outlook.com>
2023-08-06 00:56:46 +0100

    cena nova

--- php/validation.php
+++ php/validation.php
@@ -1,9 +1,12 @@
 <?php
 
+
 require 'Export.php';
 
 //validation of the form
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //validation of the form
+
     $myClass = new Export();
     $myClass->execute($_POST);
 }
