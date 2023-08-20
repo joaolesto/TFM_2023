b2e6985971bd7f4cad23a9b329603f6e96142427
joaolesto <joaolesto@outlook.com>
2023-08-20 22:53:02 +0100

    EN_HCM2000 PROGRAMADO

--- js/EN_HCM2000.js
+++ js/EN_HCM2000.js
@@ -163,74 +163,71 @@ function interpolarBilinear(x, y, x1, y1, x2, y2, q11, q12, q21, q22) {
   
 
 //passo7 fim da primeira parte
-function Ats_f(FFS,vp,fnp) {
-  return FFS-vp-fnp
+function Ats_f(FFS,vd_s,vo_s,fnp_s) {
+  return FFS-0.0125*(vd_s+vo_s)-fnp_s
 }
 
 //classe 2
 //passo 13
-function BPTSF_F(vp) {
-    return 100*(1-Math.E**(-0.000879*vp))
+function BPTSF_f(vd_ts,a,b) {
+    return 100*(1-Math.E**(a*vd_ts**(b)))
 }
 //passo 14
-function PTSF(BPTSF,fdnp) {
-  return BPTSF+fdnp
+function PTSF_f(BPTSF,fnp_ts) {
+  return BPTSF+fnp_ts
 }
 //passo15
-function class_F(classe,ATS,PTSF,vd,vo) {
+function LOS_f(classe,ATS,PTSF) {
     let LOS = ""
-    var NS = []
-    if (vd>1700 || vo>1700) {
-        LOS = "F"
-    }else {
-        if(classe==1){
-            if(ATS<=60){
-                NS[0] = 5;
-            }else if(ATS>60 && ATS<=70){
-                NS[0] = 4;
-            }else if(ATS>70 && ATS<=80){
-                NS[0] = 3;
-            }else if(ATS>80 && ATS<=90){
-                NS[0] = 2;
-            }else if(ATS>90){
-                NS[0] = 1;
-            }
-            if (PTSF<=35) {
-                NS[1] = 1;
-            }else if(PTSF>35 && PTSF<=50){
-                NS[1] = 2;
-            }else if(PTSF>50 && PTSF<=65){
-                NS[1] = 3;
-            }else if(PTSF>65 && PTSF<=80){
-                NS[1] = 4;
-            }else if(PTSF>80){
-                NS[1] = 5;
-            }
-            var max = Math.max(...NS);
-            if (max==1){
-                LOS = "A";
-            }else if(max==2){
-                LOS = "B";
-            }else if(max==3){
-                LOS = "C";
-            }else if(max==4){
-                LOS = "D";
-            }else if(max==5){
-                LOS = "E";
-            }
-        }else if(classe==2){
-            if(PTSF<=40) {
-                LOS = "A";
-            }else if(PTSF>40 && PTSF<=55){
-                LOS = "B";
-            }else if (PTSF>55 && PTSF<=70){
-                LOS = "C";
-            }else if (PTSF>70 && PTSF<=85){
-                LOS = "D";
-            }else if (PTSF>85){
-                LOS = "E"
-            }
+    var NS = [];
+    if(classe==1){
+        if(ATS<=60){
+            NS[0] = 5;
+        }else if(ATS>60 && ATS<=70){
+            NS[0] = 4;
+        }else if(ATS>70 && ATS<=80){
+            NS[0] = 3;
+        }else if(ATS>80 && ATS<=90){
+            NS[0] = 2;
+        }else if(ATS>90){
+            NS[0] = 1;
         }
-    }
+        if (PTSF<=35) {
+            NS[1] = 1;
+        }else if(PTSF>35 && PTSF<=50){
+            NS[1] = 2;
+        }else if(PTSF>50 && PTSF<=65){
+            NS[1] = 3;
+        }else if(PTSF>65 && PTSF<=80){
+            NS[1] = 4;
+        }else if(PTSF>80){
+            NS[1] = 5;
+        }
+        var max = Math.max(...NS);
+        if (max==1){
+            LOS = "A";
+        }else if(max==2){
+            LOS = "B";
+        }else if(max==3){
+            LOS = "C";
+        }else if(max==4){
+            LOS = "D";
+        }else if(max==5){
+            LOS = "E";
+        }
+    }else if(classe==2){
+        if(PTSF<=40) {
+            LOS = "A";
+        }else if(PTSF>40 && PTSF<=55){
+            LOS = "B";
+        }else if (PTSF>55 && PTSF<=70){
+            LOS = "C";
+        }else if (PTSF>70 && PTSF<=85){
+            LOS = "D";
+        }else if (PTSF>85){
+            LOS = "E"
+        }
+    } 
     return LOS
 }
+   
