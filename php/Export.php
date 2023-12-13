29017158f70566c39811ab52a3795a347c4094b7
Gustavo Silva <gustavo.silva@toogas.com>
2023-12-13 20:31:53 +0000

    exportações

--- php/Export.php
+++ php/Export.php
@@ -20,6 +20,8 @@ class Export
 
     protected array $input;
     protected array $output;
+    protected array $output2000;
+    protected array $output2022;
     protected string $filePath;
     protected string $fileName;
 
@@ -45,27 +47,62 @@ class Export
      */
     protected function uploadXlsx($post): Worksheet
     {
-        // create the content
-        $this->input = array_chunk($post['input'],2);
-        array_unshift($this->input, ['-','Inputs']);
-
-        $this->output = array_chunk($post['output'],3);
-        array_unshift($this->output, ['Outputs','HCM 2000','HCM 7ª edição']);
-
         $spreadsheet = new Spreadsheet();
         $writter = new Xlsx($spreadsheet);
 
-        // File name and its directory
-        $this->fileName = 'AEexported_'. date('d_m_Y H:i') . ".xlsx";
-        $this->filePath = "../docs/" . $this->fileName;
+        if ($post['type'] == 'ae') {
+            // create the content
+            $this->input = array_chunk($post['input'],2);
+            array_unshift($this->input, ['-','Inputs']);
 
-        // create
-        $create = $writter->getSpreadsheet()->getActiveSheet()
-            ->fromArray($this->input, null, 'B2')
-            ->fromArray($this->output, null, 'E2');
+            $this->output = array_chunk($post['output'],3);
+            array_unshift($this->output, ['Outputs','HCM 2000','HCM 7ª edição']);
+
+            // File name and its directory
+            $this->fileName = 'AEexported_'. date('d_m_Y H:i') . ".xlsx";
+            $this->filePath = "../docs/" . $this->fileName;
+
+            // create
+            $create = $writter->getSpreadsheet()->getActiveSheet()
+                ->fromArray($this->input, null, 'B2')
+                ->fromArray($this->output, null, 'E2');
+        }
+
+        if ($post['type'] == 'en') {
+            // create the content
+            $this->input = array_chunk($post['input'],2);
+            array_unshift($this->input, ['-','Inputs']);
+
+            $this->output2000 = array_chunk($post['output_2000'],2);
+            array_unshift($this->output2000, ['Outputs','HCM 2000']);
+
+            $this->output2022 = array_chunk($post['output_2022'],2);
+            array_unshift($this->output2022, ['Outputs','HCM 7ª edição']);
+
+            // File name and its directory
+            $this->fileName = 'AEexported_'. date('d_m_Y H:i') . ".xlsx";
+            $this->filePath = "../docs/" . $this->fileName;
+
+            // create
+            $create = $writter->getSpreadsheet()->getActiveSheet()
+                ->fromArray($this->input, null, 'B2')
+                ->fromArray($this->output2000, null, 'E2')
+                ->fromArray($this->output2022, null, 'H2')
+                ->calculateColumnWidths();
+        }
+
+        // validate create
+        if (empty($create)) {
+            return $writter->getSpreadsheet()->getActiveSheet();
+        }
+
+        // format columns
+        foreach ($create->getColumnIterator() as $column) {
+            $writter->getSpreadsheet()->getActiveSheet()
+                ->getColumnDimension($column->getColumnIndex())->setAutoSize(true);
+        }
 
         $writter->save($this->filePath);
-
         return $create;
     }
 
